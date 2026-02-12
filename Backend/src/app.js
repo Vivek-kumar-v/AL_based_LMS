e05781a8be40ca1d828055bbb332d166c8f4514d
Vivek-kumar-v <patelvivek6370@gmail.com>
2026-02-12 11:49:12 +0530

    cors modified

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(
     cors({
-      origin: "http://localhost:5173", // Vite frontend
+      origin: process.env.CORS_ORIGIN, // Vite frontend
       credentials: true
     })
   );
